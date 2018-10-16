7193f1e69fbc49240eadd9278b3a276a56cb79f3
guodp <guodp9u0@gmail.com>
2018-10-16 16:32:50 +0800

    update ex 2-58

---
+++ ch2/src/ex2-58.c
@@ -0,0 +1,20 @@
+/*
+小端输出 1 大端输出 0
+*/
+
+#include <stdio.h>
+#include <assert.h>
+
+typedef unsigned char *byte_pointer;
+
+int is_little_endian()
+{
+    int test = 0xff;
+    byte_pointer chars = (byte_pointer)&test;
+    return chars[0] == 0xff;
+}
+
+int main(int argc, char* argv[]) {
+  printf("%d",is_little_endian());
+  return 0;
+}
