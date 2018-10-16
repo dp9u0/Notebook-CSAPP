7c3876e8e869531952262aebab061c463ae3bbe7
guodp <guodp9u0@gmail.com>
2018-10-16 13:47:39 +0800

    update ch2.2

---
+++ ch2/src/ex2-7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef unsigned char *byte_pointer;
+
+extern void show_bytes(byte_pointer start, int len);
+
+int main(void)
+{
+        const char *s = "abcdef";
+        show_bytes((byte_pointer)s, strlen(s));
+        return 0;
+}
