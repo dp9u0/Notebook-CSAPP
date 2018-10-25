be64d01969486a53825b481e5fb0e96f542091de
guodengpeng <guodp9u0@gmail.com>
2018-10-25 16:18:42 +0800

    update ch3

--- ch3/src/3.08.3.c
+++ ch3/src/3.08.3.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
+ 
 unsigned long long getbuf()
 {
-    char buf[36];
-    volatile char *variable_length;
-    int i;
-    unsigned long long val;
-    return val % 40;
+  char buf[36];
+  volatile char* bufp = &buf;
+  buf[0] = 1;
+  int i = 2;
+  unsigned long long val = buf; 
+  printf("%llx %llx %llx %llx\n", bufp, i, val, *bufp); 
+  return val % 40;
 }
-
+ 
 int main()
 {
     getbuf();
--- ch3/src/3.08.3.s
+++ ch3/src/3.08.3.s
@@ -1,4 +1,7 @@
         .file   "main.c"
+        .section        .rodata
+.LC0:
+        .string "%llx %llx %llx %llx\n"
         .text
         .globl  getbuf
         .type   getbuf, @function
@@ -10,10 +13,28 @@ getbuf:
         .cfi_offset 6, -16
         movq    %rsp, %rbp
         .cfi_def_cfa_register 6
-        subq    $64, %rsp
+        subq    $80, %rsp
         movq    %fs:40, %rax
         movq    %rax, -8(%rbp)
         xorl    %eax, %eax
+        leaq    -48(%rbp), %rax
+        movq    %rax, -64(%rbp)
+        movb    $1, -48(%rbp)
+        movb    $1, -47(%rbp)
+        movl    $2, -68(%rbp)
+        leaq    -48(%rbp), %rax
+        movq    %rax, -56(%rbp)
+        movq    -64(%rbp), %rax
+        movzbl  (%rax), %eax
+        movsbl  %al, %esi
+        movq    -56(%rbp), %rcx
+        movl    -68(%rbp), %edx
+        movq    -64(%rbp), %rax
+        movl    %esi, %r8d
+        movq    %rax, %rsi
+        movl    $.LC0, %edi
+        movl    $0, %eax
+        call    printf
         movq    -56(%rbp), %rcx
         movabsq $-3689348814741910323, %rdx
         movq    %rcx, %rax
@@ -26,8 +47,8 @@ getbuf:
         subq    %rax, %rcx
         movq    %rcx, %rdx
         movq    %rdx, %rax
-        movq    -8(%rbp), %rsi
-        xorq    %fs:40, %rsi
+        movq    -8(%rbp), %rdi
+        xorq    %fs:40, %rdi
         je      .L3
         call    __stack_chk_fail
 .L3:
